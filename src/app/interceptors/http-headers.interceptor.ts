099bd7497960b1ab3a6548bb61ad31fe3d1c09f9
tomadragosstefan <36372955+tomadragosstefan@users.noreply.github.com>
2023-09-25 19:54:18 +0300

    Update http-headers.interceptor.ts

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -12,7 +12,7 @@ export class HttpHeadersInterceptor implements HttpInterceptor{
     ): Observable<HttpEvent<any>> {
         req = req.clone({
             setParams: {
-                key: 'you will have to get this'
+                key: 'aeb88cbe6494433c9f579ee32987b4c6'
             }
         });
 
